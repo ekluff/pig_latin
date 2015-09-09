89c5c8b39a7b3b02a91fe75997f9f7cef0969cc2
Bunker & Evan Clough <student@epicodus.com>
2015-09-09 10:21:54 -0700

    add a method to put -ay- at the end of words starting with vowels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 var pigLatin  = function (word) {
-  var result = [];
-  return result = word.split('');
+  var splitWord = [];
+  splitWord = word.split('');
+  var splitWordSlice = splitWord.slice();
 
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
 
+  i = null;
+  for (i=0; i<vowels.length; i++) {
+    if (splitWord[0] === vowels[i]) {
+      splitWordSlice.push('a', 'y')
+    };
+  };
+
+  var ordway = splitWordSlice.join('');
+  return ordway;
 
 };
 
